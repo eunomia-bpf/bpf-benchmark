550152911aee2c7e8caf8651c99598fb6691d9d7
yunwei37 <1067852565@qq.com>
2023-09-24 03:17:07 +0000

    update image

--- example-output/plot.py
+++ example-output/plot.py
@@ -21,22 +21,23 @@ for exe, exe_results in zip(executables, results):
         plot_data[test["name"]]["exec_usage"].append(test["exec_usage"])
         plot_data[test["name"]]["jit_usage"].append(test["jit_usage"])
 
-num_tests = len(plot_data)
+num_runtimes = len(executables)
 
 # Plot Execution Usage
 plt.figure(figsize=(15, 10))
 bar_width = 0.10
-total_width = num_tests * bar_width
-indices = np.arange(len(executables))
+total_width = num_runtimes * bar_width
+indices = np.arange(len(plot_data))
 
-for i, (name, values) in enumerate(plot_data.items()):
-    plt.bar(indices + i * bar_width, values["exec_usage"], width=bar_width, label=name)
+for i, exe in enumerate(executables):
+    exec_usages = [plot_data[test_name]["exec_usage"][i] for test_name in plot_data]
+    plt.bar(indices + i * bar_width, exec_usages, width=bar_width, label=exe)
 
-plt.title("Execution Usage for Different Tests Across Executables")
-plt.xlabel("Executables")
-plt.ylabel("Execution Usage (Time, Log Scale)")
+plt.title("Execution Usage for Different Tests Across Runtimes")
+plt.xlabel("Test Cases")
+plt.ylabel("Execution Usage (Time in nanoseconds, Log Scale)")
 plt.yscale("log")
-plt.xticks(indices + total_width / 2 - bar_width / 2, executables, rotation=45)
+plt.xticks(indices + total_width / 2 - bar_width / 2, plot_data.keys(), rotation=45)
 plt.legend()
 plt.grid(True, which="both", ls="--", c="0.65")
 plt.tight_layout()
@@ -45,14 +46,15 @@ plt.savefig("execution_usage_bar_plot.png")
 # Plot JIT Usage
 plt.figure(figsize=(15, 10))
 
-for i, (name, values) in enumerate(plot_data.items()):
-    plt.bar(indices + i * bar_width, values["jit_usage"], width=bar_width, label=name)
+for i, exe in enumerate(executables):
+    jit_usages = [plot_data[test_name]["jit_usage"][i] for test_name in plot_data]
+    plt.bar(indices + i * bar_width, jit_usages, width=bar_width, label=exe)
 
-plt.title("JIT Usage for Different Tests Across Executables")
-plt.xlabel("Executables")
-plt.ylabel("JIT Usage (Time, Log Scale)")
+plt.title("JIT Usage for Different Tests Across Runtimes")
+plt.xlabel("Test Cases")
+plt.ylabel("JIT Usage (Time in nanoseconds, Log Scale)")
 plt.yscale("log")
-plt.xticks(indices + total_width / 2 - bar_width / 2, executables, rotation=45)
+plt.xticks(indices + total_width / 2 - bar_width / 2, plot_data.keys(), rotation=45)
 plt.legend()
 plt.grid(True, which="both", ls="--", c="0.65")
 plt.tight_layout()
