e4534875d502d064c1dee9bce81a7050103f56d5
officeyutong <yt.xyxx@gmail.com>
2024-04-21 21:32:08 +0800

    Update

--- run_benchmark.py
+++ run_benchmark.py
@@ -9,11 +9,11 @@ import json
 
 PROGS_DIR = Path("./bpf_progs")
 EXECUTABLES = [
-    "ubpf-jit",
-    "llvm-jit",
-    "rbpf-jit",
-    "rbpf-jit-vm",
-    "ubpf-jit-vm",
+    "./ubpf-jit",
+    "./llvm-jit",
+    "./rbpf-jit",
+    "./rbpf-jit-vm",
+    "./ubpf-jit-vm",
     "<NATIVE>",
     "<WASM>",
 ]
